c4f992207d60afcc589a8d9a9df7b1cea8ab79ff
NotNuT <nutnutnut12079@hotmail.com>
2018-11-05 22:48:21 +0700

    Edit side bar text

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
     <h3 class="w3-wide"><b>NJ Network Devices</b></h3>
   </div>
   <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
-    <a href="#" class="w3-bar-item w3-button">Menu</a>
+    <a href="#" class="w3-bar-item w3-button">Router Modem</a>
   </div>
  
   
