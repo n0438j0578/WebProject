3846cfe9145a1ca6ea3420d9fb092c80551602e9
notnuttt <nutnutnut12079@hotmail.com>
2018-11-09 12:50:45 +0700

    force commit

--- conn.php
+++ conn.php
@@ -1,14 +1,23 @@
 <?php
-	$host = "localhost";
-	$user = "root";
-	$pass = "P@ssword";
-	$db = "N&N_Cafe";
+	// $host = "localhost";
+	// $user = "root";
+	// $pass = "P@ssword";
+	// $db = "N&N_Cafe";
 
-	$conn = new mysqli($host, $user, $pass, $db);
-	if($conn->connect_error){
-		die('Connection Failed: ' . $conn->connect_error );
+	// $conn = new mysqli($host, $user, $pass, $db);
+	// if($conn->connect_error){
+	// 	die('Connection Failed: ' . $conn->connect_error );
+	// }
+
+	$connect  = new MongoClient("mongodb://localhost:27017");
+	$db = $connect->Reservation;
+	$collection = $db->Users;
+
+	$cursor = $collection->find();
+
+	foreach($cursor as $result){
+		print_r($result);
 	}
-
 
 	//	$conn = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 	//	echo "Connection Successfull";
